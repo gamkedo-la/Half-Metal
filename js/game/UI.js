05d306b8a4647f65f05a5c3c4f3e966f4e332aeb
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2021-12-04 12:49:56 -0600

    Setting up test level area

--- js/game/UI.js
+++ js/game/UI.js
@@ -6,6 +6,6 @@ function UIClass(x, y, width, height) {
 
     this.draw = function () {
         colorRect(0, this.y, this.backgroundWidth, this.backgroundHeight, "black");
-        renderFont("TEST LEVEL", this.x + 20, this.y + this.backgroundHeight/2);
+        renderFont("TEST AREA", this.x + 20, this.y + 12);
     }
 }
