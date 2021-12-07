b0fdf14960f31c12a2fa17ea73376a8881149220
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2021-12-06 20:53:17 -0600

    Set up constants file

---
+++ js/general/Constants.js
@@ -0,0 +1,13 @@
+// SHOT TYPES
+const NORMAL = "normal";
+const STUN = "stun";
+const PUSH = "push";
+const TURN = "turn";
+
+// PLAYER/ENEMY STATES
+const SHOOTING = "shooting";
+const MOVING = "moving";
+const IDLE = "idle";
+const ALERT = "alert";
+const STUNNED = "stunned";
+const PUSHED = "pushed";
