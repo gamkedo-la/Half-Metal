d89c8c85f7d8571270bee42ea29f499a4c7b6190
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2021-12-09 16:30:34 -0600

    Refactored Editor.js to use ButtonClass

--- js/game/Editor.js
+++ js/game/Editor.js
@@ -1,20 +1,20 @@
 function ButtonClass(
   height = 12,
-  width = 16,
-  x,
-  y,
+  width = 10,
+  x = 0,
+  y = 0,
   label = "TEXT",
   color = "black",
   textColor = "white",
   handler,
-  paddingX = 0,
-  paddingY = 0
+  paddingX = 2,
+  paddingY = 2
 ) {
+  this.label = label;
   this.height = height;
-  this.width = width;
+  this.width = this.label.length * width;
   this.x = x;
   this.y = y;
-  this.label = label;
   this.color = color;
   this.handler = handler;
   this.paddingX = paddingX;
@@ -23,84 +23,47 @@ function ButtonClass(
 
   this.draw = function () {
     colorRect(this.x, this.y, this.width, this.height, this.color);
-    colorText(this.label, this.x + paddingX, this.y + paddingY, this.textColor);
+    renderFont(this.label, this.x + this.paddingX, this.y + this.paddingY);
   };
-  this.buttonHandler = function () {};
 }
 
 function EditorClass() {
   this.currentLevel = 0;
   this.currentMap = [];
   this.toolBarOptions = [
-    {
-      label: "NEW",
-      handler: () => {
-        this.currentMap = [...baseLevel];
-        loadLevel(this.currentMap);
-      },
-    },
-    {
-      label: "LOAD",
-      handler: () => {
-        console.log("Clicked LOAD");
-      },
-    },
-    {
-      label: "SAVE",
-      handler: () => {
-        console.log("Clicked SAVE");
-      },
-    },
-    {
-      label: "PLAY",
-      handler: () => {
-        currentMode = PLAY_MODE;
-      },
-    },
+    new ButtonClass(...[, , , ,], "NEW", ...[, ,], () => {
+      this.currentMap = [...baseLevel];
+      loadLevel(this.currentMap);
+    }),
+    new ButtonClass(...[, , , ,], "LOAD", ...[, ,], () => {
+      console.log("Clicked LOAD");
+    }),
+    new ButtonClass(...[, , , ,], "SAVE", ...[, ,], () => {
+      console.log("Clicked SAVE");
+    }),
+    new ButtonClass(...[, , , ,], "PLAY", ...[, ,], () => {
+      currentMode = PLAY_MODE;
+    }),
   ];
   this.paletteOptions = [
-    {
-      label: "ENEMY",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked ENEMIES");
-      },
-    },
-    {
-      label: "SHOTS",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked SHOTS");
-      },
-    },
-    {
-      label: "WALLS",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked WALLS");
-      },
-    },
-    {
-      label: "HAZARD",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked HAZARD");
-      },
-    },
-    {
-      label: "TILES",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked TILES");
-      },
-    },
-    {
-      label: "PLAYER",
-      pallete: [],
-      handler: () => {
-        console.log("Clicked PLAYER");
-      },
-    },
+    new ButtonClass(...[, , , ,], "ENEMY", ...[, ,], () => {
+      console.log("Clicked ENEMIES");
+    }),
+    new ButtonClass(...[, , , ,], "SHOTS", ...[, ,], () => {
+      console.log("Clicked SHOTS");
+    }),
+    new ButtonClass(...[, , , ,], "WALLS", ...[, ,], () => {
+      console.log("Clicked WALLS");
+    }),
+    new ButtonClass(...[, , , ,], "HAZARD", ...[, ,], () => {
+      console.log("Clicked HAZARD");
+    }),
+    new ButtonClass(...[, , , ,], "TILES", ...[, ,], () => {
+      console.log("Clicked TILES");
+    }),
+    new ButtonClass(...[, , , ,], "PLAYER", ...[, ,], () => {
+      console.log("Clicked PLAYER");
+    }),
   ];
   this.currentPalette = [];
   this.showLevelNameModal = false;
@@ -121,64 +84,32 @@ function EditorClass() {
   this.showEditor = false;
 
   this.initiateUI = function () {
-    this.toolBarOptions.forEach(function (option) {
-      option.height = 12;
-      option.width = option.label.length * 10;
-    });
-
-    this.paletteOptions.forEach(function (option) {
-      option.height = 12;
-      option.width = option.label.length * 10;
-    });
-  };
-
-  this.update = function () {};
-
-  this.drawButtonBackgroundLayer = function () {
-    //   TOOLBAR
     var buttonX = 0;
     var buttonY = 0;
     this.toolBarOptions.forEach((option) => {
       option.y = buttonY;
       option.x = buttonX;
-      colorRect(buttonX, option.y, option.width, 12, "black");
       buttonX = buttonX + option.label.length * 12;
     });
 
-    //   PALETTE OPTIONS
-    var paletteX = 200;
-    var paletteY = 0;
+    buttonX = 200;
+    buttonY = 0;
     this.paletteOptions.forEach(function (option) {
-      option.x = paletteX;
-      option.y = paletteY;
-      colorRect(option.x, paletteY, 56, option.height, "black");
-      paletteY += option.height;
+      option.x = buttonX;
+      option.y = buttonY;
+      buttonY += option.height;
+      option.width = 56;
     });
   };
 
-  this.drawButtonTextLayer = function () {
-    var textYOffset = 2;
-    var textXOffset = 2;
-
-    //   TOOLBAR
-    var buttonX = 0;
-    var buttonY = 0;
-    this.toolBarOptions.forEach((option) => {
-      renderFont(option.label, buttonX + textXOffset, buttonY + textYOffset);
-      buttonX = buttonX + option.label.length * 12;
-    });
-
-    //   PALETTE OPTIONS
-    var paletteX = 200;
-    var paletteY = 0;
-    this.paletteOptions.forEach(function (option) {
-      renderFont(option.label, paletteX + textXOffset, paletteY + textYOffset);
-      paletteY += option.height;
-    });
-  };
-
+  this.update = function () { };
+  
   this.draw = function () {
-    this.drawButtonBackgroundLayer();
-    this.drawButtonTextLayer();
+    this.toolBarOptions.forEach((option) => {
+      option.draw();
+    });
+    this.paletteOptions.forEach((option) => {
+      option.draw();
+    });
   };
 }
