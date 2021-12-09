c183b35ef6191d604db2ce47110b5f9c9c5834a8
Rodrigo Bonzerr S. Lopez <44669373+rodprogramdev@users.noreply.github.com>
2021-12-09 11:08:07 +0800

    able to make hunterBot move to the right - WIP

--- js/characters/Hunter.js
+++ js/characters/Hunter.js
@@ -1,17 +1,20 @@
 HunterClass.prototype = new enemyClass();
 
+const HUNTER_BOT_MOVEMENT_SPEED=1.0;
+
 function HunterClass() {
   this.myTileKind = TILE_HUNTER;
   this.x = 75;
   this.y = 75;
+  this.speedX = HUNTER_BOT_MOVEMENT_SPEED;
+  this.speedY = HUNTER_BOT_MOVEMENT_SPEED;
   this.velX=1;
   this.velY=2;
   this.width = 35;
   this.height = 54;
   this.image = hunterBotImage;
   this.angleMovement = 0;
-  this.life;
-
+  
   this.reset = function (whichImage) {
     this.image = whichImage;
 
@@ -31,15 +34,18 @@ function HunterClass() {
 
   
   this.move = function (){
-    nextX = this.x;
-    nextY = this.y;
+    // nextX = this.x;
+    // nextY = this.y;
+
+    this.x += this.speedX;
+
 
     var hunterPosition; 
     hunterPosition = new HunterClass();
     hunterPosition.x = player.x;
     hunterPosition.y = player.y;
-    hunterPosition.velX = 6 - Math.random() * 10;
-    hunterPosition.velY = 10 - Math.random() * 10;
+    // hunterPosition.speedX = 6 - Math.random() * 10;
+    // hunterPosition.speedY = 10 - Math.random() * 10;
   }
 
   this.draw = function () {
