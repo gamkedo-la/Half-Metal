d7f6632778ff5f8e8396b529e0c9deac6fe8047f
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2021-12-12 00:22:14 -0600

    Created back hotbox for blocker

--- js/characters/Blocker.js
+++ js/characters/Blocker.js
@@ -5,6 +5,10 @@ const BLOCKER_BOT_MOVEMENT_SPEED = 1.0;
 function BlockerClass() {
   this.myTileKind = TILE_BLOCKER;
   this.type = BLOCKER;
+  this.back_hitbox_width = 0;
+  this.back_hitbox_height = 0;
+  this.back_hitbox_x = 0;
+  this.back_hitbox_y = 0;
   this.width = 34;
   this.height = 34;
   this.image = blockerSheet;
@@ -13,6 +17,7 @@ function BlockerClass() {
     idle: [{ x: 0, y: 0, w: this.width, h: this.height }],
   };
   this.currentAnimation = "idle";
+  this.render_hitbox = true;
 
   this.animator = new SpriteSheetAnimatorClass(this);
 
@@ -33,9 +38,82 @@ function BlockerClass() {
     console.log("NO BLOCKERBOT START FOUND!");
   };
 
+  this.updateHitBoxDimensions = function () {
+    switch (DIRECTION_MAP[this.direction]) {
+      case UP:
+      case DOWN:
+        this.back_hitbox_height = this.height / 2;
+        this.back_hitbox_width = this.width;
+        break;
+      case LEFT:
+      case RIGHT:
+        this.back_hitbox_height = this.height;
+        this.back_hitbox_width = this.width / 2;
+        break;
+    }
+  };
+
+  this.updateHitBoxPosition = function () {
+    switch (DIRECTION_MAP[this.direction]) {
+      case UP:
+        this.back_hitbox_x = this.x - this.width / 2;
+        this.back_hitbox_y = this.y;
+        break;
+
+      case DOWN:
+        this.back_hitbox_x = this.x - this.width / 2;
+        this.back_hitbox_y = this.y - this.height / 2;
+        break;
+
+      case LEFT:
+        this.back_hitbox_x = this.x;
+        this.back_hitbox_y = this.y - this.height / 2;
+        break;
+        
+        case RIGHT:
+          this.back_hitbox_x = this.x - this.width / 2;
+          this.back_hitbox_y = this.y - this.height / 2;
+        break;
+
+      default:
+        this.back_hitbox_x = this.x;
+        this.back_hitbox_x = this.y;
+        break;
+    }
+  };
+
   this.update = function () {
+    this.updateHitBoxDimensions();
+    this.updateHitBoxPosition();
     if (this.health <= 0) {
       this.removeSelf();
+    }
+  };
+
+  this.draw = function () {
+    this.raycast();
+    canvasContext.lineWidth = 1;
+    canvasContext.strokeStyle = "red";
+    canvasContext.beginPath();
+    canvasContext.moveTo(this.x, this.y);
+
+    this.rays.forEach(function (ray) {
+      ray.draw();
+      canvasContext.lineTo(ray.x, ray.y);
+    });
+
+    canvasContext.stroke();
+
+    this.animator.animate();
+
+    if (this.render_hitbox) {
+      colorRect(
+        this.back_hitbox_x,
+        this.back_hitbox_y,
+        this.back_hitbox_width,
+        this.back_hitbox_height,
+        "red"
+      );
     }
   };
 }
