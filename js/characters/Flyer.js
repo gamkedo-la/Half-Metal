6a59f79188f4473222411f777951bd0417421ab6
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2021-12-31 15:25:33 -0600

    Flyer robot propeller animation

--- js/characters/Flyer.js
+++ js/characters/Flyer.js
@@ -8,7 +8,7 @@ function FlyerClass() {
   this.hitbox_width = this.width;
   this.hitbox_height = this.height;
   this.image = flyer_sheet;
-  this.currentAnimation = "fly-right";
+  this.currentAnimation = "fly-down";
   this.animations = {
     "fly-down": [
       { x: 0, y: 0, w: this.width, h: this.height },
@@ -26,6 +26,11 @@ function FlyerClass() {
       { x: 0, y: 48, w: this.width, h: this.height },
       { x: 16, y: 48, w: this.width, h: this.height },
     ],
+  };
+
+  this.reset = function () {
+    this.animator = new SpriteSheetAnimatorClass(this, 4);
+    resetGameObject(this);
   };
 
   this.move = function () {};
