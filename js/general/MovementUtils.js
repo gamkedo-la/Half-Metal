471645e17c1a60f9a5bd84a03d49c46e69fc62fd
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-01-19 23:53:03 -0600

    Util for getting an arc with flat peaks

--- js/general/MovementUtils.js
+++ js/general/MovementUtils.js
@@ -22,3 +22,14 @@ function applyVector(object, vector) {
   object.x += vector.magnitude * Math.cos((vector.direction * Math.PI) / 180);
   object.y += vector.magnitude * Math.sin((vector.direction * Math.PI) / 180);
 }
+
+function getFlatArc(input, delay, amplitude) {
+  return (
+    Math.sqrt(
+      (1 + Math.pow(delay, 2)) /
+        (1 + Math.pow(delay, 2) * Math.pow(Math.cos(input), 2))
+    ) *
+    Math.cos(input) *
+    amplitude
+  );
+}
