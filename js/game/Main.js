07641fb5e499bd75e5287420f71e0587625edd88
cookieoverflow <hey@cookieoverflow.com>
2022-02-05 01:02:50 +0000

    Practice commmit - will remove

--- js/game/Main.js
+++ js/game/Main.js
@@ -21,6 +21,7 @@ var ui;
 var test_prompt = new TutorialPromptClass({
   prompt: TUTORIAL_TEXT[1],
 });
+// Practice comment from cookieoverflow, will remove
 
 // TUTORIAL PROMPTS
 var walk_prompt = new TutorialPromptClass({
