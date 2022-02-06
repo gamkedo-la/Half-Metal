ca2e8d7cfccbba65a14f2a88300671da14f101f3
Raik Bradley <69724996+EnglishTeacherSegovia@users.noreply.github.com>
2022-02-06 18:41:06 +0000

    added options to menu

--- js/game/Title.js
+++ js/game/Title.js
@@ -1,40 +1,42 @@
 function TitleMenu() {
-  // BUTTONS
-  const new_game = new ButtonClass(...[, , , ,], "NEW GAME", ...[, ,], () => {
-    console.log("Clicked new game");
-    currentMode = 'PLAY_MODE';
-  });
+    // BUTTONS
+    const new_game = new ButtonClass(...[, , , , ], "NEW GAME", ...[, , ], () => {
+        console.log("Clicked new game");
+        currentMode = 'PLAY_MODE';
+    });
 
-  const select_level = new ButtonClass(
-    ...[, , , ,],
-    "select level",
-    ...[, ,],
-    () => {
-      console.log("Clicked select level");
-    }
-  );
+    const select_level = new ButtonClass(
+        ...[, , , , ],
+        "select level",
+        ...[, , ],
+        () => {
+            console.log("Clicked select level");
+        }
+    );
 
-  const options = new ButtonClass(...[, , , ,], "options", ...[, ,], () => {
-    console.log("Clicked options");
-  });
+    const options = new ButtonClass(...[, , , , ], "options", ...[, , ], () => {
+        console.log("Clicked options");
+        currentMode = MENU_MODE;
+        menu_stack.push(options_screen);
+    });
 
-  const credits = new ButtonClass(...[, , , ,], "credits", ...[, ,], () => {
-    console.log("Clicked credits");
-  });
+    const credits = new ButtonClass(...[, , , , ], "credits", ...[, , ], () => {
+        console.log("Clicked credits");
+    });
 
-  const controls = new ButtonClass(...[, , , ,], "controls", ...[, ,], () => {
-    console.log("Clicked controls");
-  });
+    const controls = new ButtonClass(...[, , , , ], "controls", ...[, , ], () => {
+        console.log("Clicked controls");
+    });
 
-  // CONFIGURATION
-  const title_config = {
-    name: "Title",
-    buttons: [new_game, select_level, options, credits, controls],
-    button_start_x: 87,
-    button_start_y: 160,
-    image: title_img,
-  };
+    // CONFIGURATION
+    const title_config = {
+        name: "Title",
+        buttons: [new_game, select_level, options, credits, controls],
+        button_start_x: 87,
+        button_start_y: 160,
+        image: title_img,
+    };
 
-  // SUPER CONSTRUCTOR
-  MenuClass.call(this, title_config);
+    // SUPER CONSTRUCTOR
+    MenuClass.call(this, title_config);
 }
