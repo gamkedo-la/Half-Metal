003e562bda87788d5cc51c1704d0360248180cd7
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-02-12 04:09:57 -0600

    Floor counter in UI

--- js/game/UI.js
+++ js/game/UI.js
@@ -12,6 +12,7 @@ function UIClass(x, y, width, height) {
     [PUSH]: "push",
   };
   this.speedrun_timer = new SpeedRunClockClass();
+  this.floor_levels = [...CELL_LEVELS, ...ARMORY_LEVELS, ...PROCESSING_LEVELS];
 
   this.update = function () {
     if (speedrun_mode) {
@@ -27,16 +28,26 @@ function UIClass(x, y, width, height) {
     colorRect(0, this.y, this.backgroundWidth, this.backgroundHeight, "black");
 
     if (currentMode === PLAY_MODE) {
+      // AMMO COUNTER
       renderFont("AMMO ", this.x_buffer, this.text_height);
       renderFont(
         player.ammo.toString(10),
         this.x_buffer + 40,
         this.text_height
       );
-      renderFont("TYPE ", this.x_buffer + 80, this.text_height);
+
+      // AMMO SELECTION
+      renderFont("TYPE ", this.x_buffer + 60, this.text_height);
       renderFont(
         this.ammo_type_map[player.selected_ammo],
-        this.x_buffer + 120,
+        this.x_buffer + 100,
+        this.text_height
+      );
+
+      // FLOOR COUNT
+      renderFont(
+        `FLOOR ${this.floor_levels.length - currentLevel}`,
+        this.x_buffer + 170,
         this.text_height
       );
     }
