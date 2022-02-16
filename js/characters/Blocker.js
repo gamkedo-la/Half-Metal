6fa228d2fdbcfb9cfd16e1f5e46613df09330777
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-02-15 21:01:39 -0600

    Fixing blocker AI errors

--- js/characters/Blocker.js
+++ js/characters/Blocker.js
@@ -1,4 +1,5 @@
-const BLOCKER_BOT_MOVEMENT_SPEED = 1.0;
+const BLOCKER_BOT_MOVEMENT_SPEED = 0.3;
+const SHOOT_COUNTDOWN_MAX = 40;
 
 function BlockerClass() {
   EnemyClass.call(this);
@@ -14,129 +15,29 @@ function BlockerClass() {
   this.speed = BLOCKER_BOT_MOVEMENT_SPEED;
   this.animations = FRAME_DATA[BLOCKER];
   this.currentAnimation = "idle-right";
-  this.shot_timer = new TimerClass(
-    () => {
+  this.shoot_timer = SHOOT_COUNTDOWN_MAX;
+
+  this.whileAlerted = function () {
+    // Override in subclasses
+    this.alert_timer.update();
+    this.shoot_timer -= 1;
+    if (this.shoot_timer <= 0) {
       this.shoot();
-    },
-    1000,
-    3,
-    false
-  );
-  this.shield_timer = new TimerClass(
-    () => {
-      this.raiseShield();
-    },
-    1000,
-    1,
-    false
-  );
-  this.alert_timer = new TimerClass(
-    () => {
-      this.stopAlert();
-    },
-    10000,
-    1,
-    false
-  );
-  this.shield_up = false;
+      this.shoot_timer = SHOOT_COUNTDOWN_MAX;
+    }
+  };
 
   this.move = function () {
+    if (this.state === ALERT) {
+      return;
+    }
     strafe(this);
     this.updateHitBoxes();
   };
-  // State
-  this.alerted = function (dt) {
-    this.speed = 0;
-    this.alert_timer.start();
-    this.shot_timer.start();
-    this.shot_timer.update();
-    this.shield_timer.update();
-
-    if (this.shot_timer.finished) {
-      this.shield_timer.start();
-    }
-
-    if (this.shield_timer.finished) {
-      this.shot_timer.reset();
-      this.shield_timer.stop();
-    }
-  };
-
-  this.stopAlert = function () {
-    this.shot_timer.stop();
-    this.shield_timer.stop();
-    this.alert_timer.stop();
-    this.state = MOVING;
-    this.speed = BLOCKER_BOT_MOVEMENT_SPEED;
-  };
-
-  // Collision
-  this.updateHitBoxDimensions = function () {
-    switch (DIRECTION_MAP[this.direction]) {
-      case UP:
-      case DOWN:
-        this.back_hitbox_height = this.height / 2;
-        this.back_hitbox_width = this.width;
-        break;
-      case LEFT:
-      case RIGHT:
-        this.back_hitbox_height = this.height;
-        this.back_hitbox_width = this.width / 2;
-        break;
-    }
-  };
-
-  this.updateHitBoxPosition = function () {
-    switch (DIRECTION_MAP[this.direction]) {
-      case UP:
-        this.back_hitbox_x = this.x - this.width / 2;
-        this.back_hitbox_y = this.y;
-        break;
-
-      case DOWN:
-        this.back_hitbox_x = this.x - this.width / 2;
-        this.back_hitbox_y = this.y - this.height / 2;
-        break;
-
-      case LEFT:
-        this.back_hitbox_x = this.x;
-        this.back_hitbox_y = this.y - this.height / 2;
-        break;
-
-      case RIGHT:
-        this.back_hitbox_x = this.x - this.width / 2;
-        this.back_hitbox_y = this.y - this.height / 2;
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  this.updateHitBoxes = function () {
-    this.updateHitBoxDimensions();
-    this.updateHitBoxPosition();
-    this.hitbox_x = this.x - this.width / 2;
-    this.hitbox_y = this.y - this.height / 2;
-    this.hitbox_height = this.height;
-    this.hitbox_width = this.width;
-  };
-
-  this.onMoveInOpenSpace = function () {
-    this.animator.setAnimation(`walk-${getDirectionConstantOfObject(this)}`);
-    strafe(this);
-  };
-
-  this.onCollideWithSolid = function () {
-    reverseDirection(this);
-    strafe(this);
-  };
-
-  this.onCollideWithDestructible = this.onCollideWithSolid;
 
   // Class Specialties
   this.shoot = function () {
-    this.currentAnimation = `idle-${getDirectionConstantOfObject(this)}`;
+    // this.currentAnimation = `idle-${getDirectionConstantOfObject(this)}`;
     var spawn_x = this.width * Math.cos((this.direction * Math.PI) / 180);
     var spawn_y = this.height * Math.sin((this.direction * Math.PI) / 180);
 
@@ -144,10 +45,5 @@ function BlockerClass() {
     playSound(sounds.shoot);
 
     this.shield_up = false;
-  };
-
-  this.raiseShield = function () {
-    this.animator.setAnimation(`shield-${getDirectionConstantOfObject(this)}`);
-    this.shield_up = true;
   };
 }
