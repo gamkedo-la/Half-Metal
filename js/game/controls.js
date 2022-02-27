c32e0c0658940a0ddf3cef7c57e513a19e5cc128
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-02-27 13:40:41 -0600

    Adding slash to font set

--- js/game/controls.js
+++ js/game/controls.js
@@ -1,58 +1,60 @@
 function Controls() {
+  const controls_config = {
+    buttons: [
+      new ButtonClass(...[, , , ,], "Up/W Key: Move up", ...[, ,], () => {}),
+      new ButtonClass(
+        ...[, , , ,],
+        "Down/S Key: Move down",
+        ...[, ,],
+        () => {}
+      ),
+      new ButtonClass(
+        ...[, , , ,],
+        "Right/D Key: Move right",
+        ...[, ,],
+        () => {}
+      ),
+      new ButtonClass(
+        ...[, , , ,],
+        "Left/A Key: Move Left",
+        ...[, ,],
+        () => {}
+      ),
+      new ButtonClass(...[, , , ,], "Space Bar: Fire Shot", ...[, ,], () => {}),
+      new ButtonClass(
+        ...[, , , ,],
+        "X Key: change ammo Type",
+        ...[, ,],
+        () => {}
+      ),
+      new ButtonClass(
+        ...[, , , ,],
+        "BACK",
+        ...[, ,],
+        () => {
+          menu_stack.pop();
+          this.deactivateMenuButtons();
+        },
+        ...[, , ,],
+        "back"
+      ),
+    ],
+    name: "Controls",
+    button_start_x: this.button_start_x,
+    button_start_y: this.button_start_y,
+    show_name: true,
+    forceLastButton: true,
+  };
+  MenuClass.call(this, controls_config);
 
-    const controls_config = {
-        buttons: [
-            new ButtonClass(...[, , , , ], "Up Key: Move up", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "Down Key: Move down", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "Right Key: Move right", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "Left Key: Move Left", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "Space Bar: Fire Shot", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "X Key: change ammo Type", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "W Key: Move Up", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "D Key: Move Left", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "S Key: Move Down", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "A Key: Move Right", ...[, , ], () => {      
-            }),
-            new ButtonClass(...[, , , , ], "BACK", ...[, , ], () => {
-                menu_stack.pop();
-                this.deactivateMenuButtons();
-            }, ...[, , ,],
-            "back"),
-        ],
-        name: "Controls",
-        button_start_x: this.button_start_x,
-        button_start_y: this.button_start_y,
-        show_name: true,
-        forceLastButton: true,
-        
-    };
-    MenuClass.call(this, controls_config);
-    
-    this.update = function () {
-        this.updateCursor();
-        var button_y_margin = 15;
-        this.buttons.forEach((button, index) => {
-          button.active = true;
-          button.x = this.button_start.x + 10;
-          button.y = (this.button_start.y + 40) + button.height + button_y_margin * index;
-    
-         
-        });
-    
-       
-    };
-    
-    
-
-    
-
+  this.update = function () {
+    this.updateCursor();
+    var button_y_margin = 15;
+    this.buttons.forEach((button, index) => {
+      button.active = true;
+      button.x = this.button_start.x + 10;
+      button.y =
+        this.button_start.y + 40 + button.height + button_y_margin * index;
+    });
+  };
 }
